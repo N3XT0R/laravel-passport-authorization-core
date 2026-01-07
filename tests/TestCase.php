116b04e513511d5914eda0e597d212a8eed3eb5e
Ilya Beliaev <info@php-dev.info>
2026-01-07 18:21:25 +0100

    init setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,35 +3,31 @@
 namespace N3XT0R\LaravelPassportAuthorizationCore\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use N3XT0R\LaravelPassportAuthorizationCore\LaravelPassportAuthorizationCoreServiceProvider;
+use Orchestra\Testbench\Concerns\WithWorkbench;
+use Orchestra\Testbench\TestCase as Orchestra;
+use Spatie\Activitylog\ActivitylogServiceProvider;
 
 class TestCase extends Orchestra
 {
+    use WithWorkbench;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'N3XT0R\\LaravelPassportAuthorizationCore\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn(string $modelName) => 'N3XT0R\\LaravelPassportAuthorizationCore\\Database\\Factories\\' . class_basename(
+                    $modelName
+                ) . 'Factory'
         );
     }
 
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
+            ActivitylogServiceProvider::class,
             LaravelPassportAuthorizationCoreServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-         foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/../database/migrations') as $migration) {
-            (include $migration->getRealPath())->up();
-         }
-         */
     }
 }
