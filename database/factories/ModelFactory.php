11c21aa47d9e86217d24f8ee93e32890ef00578f
Ilya Beliaev <info@php-dev.info>
2026-01-07 17:41:03 +0100

    init setup

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace N3XT0R\LaravelPassportAuthorizationCore\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace N3XT0R\LaravelPassportAuthorizationCore\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use VendorName\Skeleton\SkeletonServiceProvider;
+use N3XT0R\LaravelPassportAuthorizationCore\LaravelPassportAuthorizationCoreServiceProvider;
 
 class TestCase extends Orchestra
 {
@@ -13,14 +13,14 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'N3XT0R\\LaravelPassportAuthorizationCore\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            SkeletonServiceProvider::class,
+            LaravelPassportAuthorizationCoreServiceProvider::class,
         ];
     }
 
