b4cf53c6847c926d18c7e20ebadad90573dbff92
Ilya Beliaev <info@php-dev.info>
2026-01-10 14:52:44 +0100

    feat: added context_client_id to repo and service.

---
+++ src/Application/UseCases/Tokenables/UpsertGrantsForTokenableUseCase.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N3XT0R\LaravelPassportAuthorizationCore\Application\UseCases\Tokenables;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use N3XT0R\LaravelPassportAuthorizationCore\Events\Tokenable\TokenableGrantUpsertedEvent;
+use N3XT0R\LaravelPassportAuthorizationCore\Services\GrantService;
+use N3XT0R\LaravelPassportAuthorizationCore\Support\Resolver\GrantableTokenableResolver;
+
+readonly class UpsertGrantsForTokenableUseCase
+{
+    public function __construct(
+        protected GrantableTokenableResolver $grantableTokenableResolver,
+        protected GrantService $grantService,
+    ) {
+    }
+
+    public function execute(
+        int|string $ownerId,
+        int|string $contextClientId,
+        array $scopes,
+        ?Authenticatable $actor = null
+    ): void {
+        $context = $this->grantableTokenableResolver->resolve($ownerId, $contextClientId);
+
+        $this->grantService->upsertGrantsForTokenable(
+            tokenable: $context->tokenable,
+            scopes: $scopes,
+            actor: $actor,
+            contextClient: $context->contextClient
+        );
+
+        TokenableGrantUpsertedEvent::dispatch(
+            model: $context->tokenable,
+            scopes: $scopes,
+            contextClient: $context->contextClient,
+            actor: $actor
+        );
+    }
+}
