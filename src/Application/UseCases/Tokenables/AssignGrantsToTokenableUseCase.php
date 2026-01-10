2c729c0225863bae4f6b96873b022db209733150
Ilya Beliaev <info@php-dev.info>
2026-01-10 13:20:05 +0100

    feat: added context_client_id to repo and service.

--- src/Application/UseCases/Tokenables/AssignGrantsToTokenableUseCase.php
+++ src/Application/UseCases/Tokenables/AssignGrantsToTokenableUseCase.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace N3XT0R\LaravelPassportAuthorizationCore\Application\UseCases\Tokenables;
 
 use Illuminate\Contracts\Auth\Authenticatable;
+use N3XT0R\LaravelPassportAuthorizationCore\Exceptions\Domain\ActiveClientNotExistsException;
 use N3XT0R\LaravelPassportAuthorizationCore\Repositories\ClientRepository;
 use N3XT0R\LaravelPassportAuthorizationCore\Repositories\OwnerRepository;
 use N3XT0R\LaravelPassportAuthorizationCore\Services\GrantService;
@@ -18,8 +19,20 @@ readonly class AssignGrantsToTokenableUseCase
     ) {
     }
 
-    public function execute(int|string $ownerId, array $scopes, ?Authenticatable $actor = null)
-    {
+    public function execute(
+        int|string $ownerId,
+        int|string $contextClientId,
+        array $scopes,
+        ?Authenticatable $actor = null
+    ): void {
+        $client = $this->clientRepository->findActive($contextClientId);
+
+        if (!$client) {
+            throw new ActiveClientNotExistsException($contextClientId);
+        }
+
+        $owner = $this->ownerRepository->findByKey($ownerId);
+
         $this->clientRepository->findForUser();
     }
 }
